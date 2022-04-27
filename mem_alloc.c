c7cc6df57f1b67bd051d51c07588300f3b15d451
ecrecords <elvischino754@gmail.com>
2022-04-27 00:07:58 -0700

    ecrecords: finished print and parameter option

--- mem_alloc.c
+++ mem_alloc.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct MEM_BLOC_STRUCT MEM_BLOC;
+
+struct MEM_BLOC_STRUCT
+{
+    int id;
+    int start_addr;
+    int end_addr;
+    MEM_BLOC* next;
+
+};
+
+void init_mem(MEM_BLOC **list, int *rem_mem);
+
+void print(MEM_BLOC **list);
+
+void ff_alloc(MEM_BLOC **list, int* mem_depth);
+
+void dealloc(MEM_BLOC **list, int *mem_depth);
 
 int main(){
     int option;
+
+    int rem_mem = 0;
+    MEM_BLOC* list = NULL;
 
     printf("Memory Allocation\n"
             "-----------------\n"
@@ -14,16 +37,17 @@ int main(){
     
     while (1)
     {
-        printf("\nEnter Selection:");
+        printf("\nEnter Selection: ");
         scanf("%d", &option);
 
         if (option == 1)
         {
-            /* code */
+            init_mem(&list, &rem_mem);
         }
         else if (option == 2)
         {
-            /* code */
+            ff_alloc(&list, &rem_mem);
+            print(&list);
         }
         else if (option == 3)
         {
@@ -44,4 +68,86 @@ int main(){
     }
     
     return 0;
+}
+
+void init_mem(MEM_BLOC **list, int *rem_mem){
+    MEM_BLOC *dummy;
+
+    printf("Enter size of physical memory: ");
+    scanf("%d", rem_mem);
+    
+    dummy = (MEM_BLOC*) malloc( sizeof(MEM_BLOC) );
+
+    dummy->id = -1;
+    dummy->start_addr = 0;
+    dummy->end_addr = 0;
+    dummy->next = NULL;
+
+    *list = dummy;
+}
+
+void print(MEM_BLOC **list){
+
+    printf("\nID\tStart   End\n"
+            "-------------------\n");
+    
+    MEM_BLOC *ptr = (*list)->next;
+        printf("%2d\t%4d  %4d\n", 
+            ptr->id, 
+            ptr->start_addr, 
+            ptr->end_addr);
+        ptr = (*list)->next;
+}
+
+void ff_alloc(MEM_BLOC **list, int *rem_mem){
+    MEM_BLOC *bloc;
+    MEM_BLOC *curr;
+    int id;
+    int size;
+
+    printf("Enter block id: ");
+    scanf("%d", &(id));
+    
+    printf("Enter block size:");
+    scanf("%d", &size);
+
+    if (size > *rem_mem) {
+        printf("Not Enough Memory\n");
+        return;
+    }
+
+    bloc  = (MEM_BLOC*) malloc(sizeof(MEM_BLOC));
+
+    if ((*list)->next == NULL) {
+        bloc->id = id;
+        bloc->start_addr = 0;
+        bloc->end_addr = bloc->start_addr + (size-1);
+        bloc->next = NULL;
+        *rem_mem -= size;
+        (*list)->next = bloc;
+        return;
+    }
+
+    curr = (*list)->next;
+
+    while (curr != NULL)
+    {
+        if ( (*list)->id == bloc->id){
+            printf("Duplicate id found\n");
+            return;
+        }
+
+        if ( ((curr->next)->start_addr - curr->end_addr) >= size){
+            bloc->id = id;
+            bloc->start_addr = curr->end_addr + 1;
+            bloc->start_addr = (curr->next->start_addr) - 1;
+            bloc->next = NULL;
+            *rem_mem -= size;
+            return;
+        }        
+
+        curr = curr->next;
+    }
+    
+
 }
